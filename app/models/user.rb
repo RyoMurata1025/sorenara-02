ad3737458ae332791c658e5cb39994f92b92fcd0
RyoMurata1025 <lagoon1025.work@gmail.com>
2021-04-01 08:18:08 +0900

    usersテーブルに保存するためのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  rege_user_x = /\A[ぁ-んァ-ン一-龥]/
+  rege_user_y = /\A[ァ-ヶー－]+\z/
+
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}, confirmation: true
+    validates :family_name, format: { with: rege_user_x}
+    validates :first_name, format: { with: rege_user_x}
+    validates :family_name_kana, format: { with: rege_user_y}
+    validates :first_name_kana, format: { with: rege_user_y}
+    validates :birth_day
+  end
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/}
 end
