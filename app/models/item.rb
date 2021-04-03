3a82395cabdc99997331128946eb6176d34f4aa7
RyoMurata1025 <lagoon1025.work@gmail.com>
2021-04-04 05:53:36 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :address
+    validates :information
+    validates :category
+  end
 end
