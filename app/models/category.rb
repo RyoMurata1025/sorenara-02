f9688c19be4f3bd33a32c75e6ae425ca0beff4a3
RyoMurata1025 <lagoon1025.work@gmail.com>
2021-04-03 09:05:44 +0900

    category.rbにカテゴリーの記述とビューへのプルダウン反映

---
+++ app/models/category.rb
@@ -0,0 +1,11 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '観光' },
+    { id: 2, name: 'グルメ' },
+    { id: 3, name: 'ランチ' },
+    { id: 4, name: 'スイーツ' },
+    { id: 5, name: 'スポーツ' },
+    { id: 6, name: 'エンタメ' },
+    { id: 7, name: 'その他' }
+  ]
+end
