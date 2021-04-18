897110c74ac99bfafb5c456ccece0896eb3f506b
RyoMurata1025 <lagoon1025.work@gmail.com>
2021-04-19 06:04:17 +0900

    deploy.rbのユーザーネーム修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'sorenara-02'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:lagoon1025/sorenara-02.git'
+set :repo_url,  'git@github.com:RyoMurata1025/sorenara-02.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
